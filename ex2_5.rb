20318a8771370039eeaceeefc9fcb7b58dd73a00
IanMoreno <imorenodev@gmail.com>
2014-05-16 01:22:54 -0400

    Chapters 1 & 2

---
+++ ex2_5.rb
@@ -0,0 +1,15 @@
+#How many hours are in a year?
+hoursInYear = 365*24
+puts "There are #{ hoursInYear } hours in a year."
+
+#How many minutes are in a decade?
+minsInDecade = 10*365*24*60
+puts "There are #{ minsInDecade } minutes in a decade."
+
+#How many seconds old are you?
+secondsOld = (27*365+25)*24*60*60
+puts "I am #{ secondsOld } seconds old."
+
+#How old is the author?
+authorsAge = 1160000000/60/60/24/365
+puts "The author is #{ authorsAge } years old"
