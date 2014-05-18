9ef195a65bf12b8da20bad7943e39a12f669be48
IanMoreno <imorenodev@gmail.com>
2014-05-17 20:29:31 -0400

    end of chapter three

---
+++ strings3_2.rb
@@ -0,0 +1,13 @@
+puts 'I like ' + 'apple pie.'
+puts 'I like' + ' apple pie.'
+
+puts 'blink ' * 4
+
+puts 2 * 5
+puts '2' * 5
+puts '2 * 5'
+
+puts 'You\'re swell!'
+puts 'backslash at the end of a string: \\'
+puts 'up\\down'
+puts 'up\down'
