8309de36d1f03b198dbcf20bbcbc18b77deb62b0
IanMoreno <imorenodev@gmail.com>
2014-05-19 01:15:04 -0400

    chapter4-5

---
+++ bigger_better_fav_num5_6.rb
@@ -0,0 +1,5 @@
+puts "What is your favorite number?"
+number = gets.to_i
+puts "#{number} is your favorite number?"
+puts "May I suggest #{number+1}? It is a bigger and better favorite number."
+
---
+++ full_name_greeting5_6.rb
@@ -0,0 +1,7 @@
+puts 'Hello, what is your first name?'
+firstName = gets.chomp
+puts 'What is your middle name?'
+middleName = gets.chomp
+puts 'What is your last name?'
+lastName = gets.chomp
+puts 'Thank you ' + firstName + ' ' + middleName + ' ' + lastName + '!'
