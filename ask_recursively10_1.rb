bf85366efef8c545393dac1cb4ec205bc0c450e2
IanMoreno <imorenodev@gmail.com>
2014-06-04 00:48:27 -0400

    Chapter ten

---
+++ ask_recursively10_1.rb
@@ -0,0 +1,17 @@
+# recursive solution to the psychology ask program
+
+def ask_recursively question
+  puts question
+  reply = gets.chomp.downcase
+
+  if reply == "yes"
+    true
+  elsif reply == "no"
+    false
+  else
+    puts "Please answer \"yes\" or \"no\""
+    ask_recursively question
+  end
+end
+
+ask_recursively "Do you wet the bed?"
