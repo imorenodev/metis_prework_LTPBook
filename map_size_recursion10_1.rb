5e1ec01af7511d85028a053286ee04939adf44a5
IanMoreno <imorenodev@gmail.com>
2014-06-04 00:47:48 -0400

    Map size program using recursion

---
+++ map_size_recursion10_1.rb
@@ -0,0 +1,37 @@
+# Count the size of a continent in an 11x11 map
+M = "land"
+o = "water"
+
+world = [[o,o,o,o,o,o,o,o,o,o,o],
+         [o,o,o,o,M,M,o,o,o,o,o],
+         [o,o,o,o,o,o,o,o,M,M,o],
+         [o,o,o,M,o,o,o,o,o,M,o],
+         [o,o,o,M,o,M,M,o,o,o,o],
+         [o,o,o,o,M,M,M,M,o,o,o],
+         [o,o,o,M,M,M,M,M,M,M,o],
+         [o,o,o,M,M,o,M,M,M,o,o],
+         [o,o,o,o,o,o,M,M,o,o,o],
+         [o,M,o,o,o,M,o,o,o,o,o],
+         [o,o,o,o,o,o,o,o,o,o,o]]
+
+def continent_size world, x, y
+  if world[y][x] != "land"
+    # Either it's water or already been counted
+    return 0
+  end
+  # first we count this tile
+  size = 1
+  world[y][x] = "counted land"
+  # and then we count all of the neighboring eight tiles and their neighbors by recursion.
+  size = size + continent_size(world, x-1, y-1)
+  size = size + continent_size(world, x, y-1)
+  size = size + continent_size(world, x+1, y-1)
+  size = size + continent_size(world, x-1, y)
+  size = size + continent_size(world, x+1, y)
+  size = size + continent_size(world, x-1, y+1)
+  size = size + continent_size(world, x, y+1)
+  size = size + continent_size(world, x+1, y+1)
+  size
+end
+
+puts continent_size(world, 5, 5)
